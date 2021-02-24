c120d11a392da366962a35790f963af51aae41b3
FREEZA07 <FREEZA@777.com>
2021-02-24 18:25:40 +0800

    UPDATE: 完善文件结构

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import ReactDOM from 'react-dom';
+import React, { Fragment } from 'react';
+
+import 'antd/dist/antd.css';
+
+
+ReactDOM.render(
+  <Fragment>
+    1
+  </Fragment>,
+  document.getElementById('root')
+);
+
---
+++ src/samples/README.txt
@@ -0,0 +1,5 @@
+文件夹名称：
+components
+
+文件夹说明：
+用于存放使用antd 4.x(https://ant.design/docs/react/introduce-cn)的一些学习例子；
